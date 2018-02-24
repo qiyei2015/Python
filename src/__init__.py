eaf80bea304e55ee61fb2d2b84c1e897f56e5d65
qiyei2015 <1273482124@qq.com>
2018-02-24 21:41:03 +0800

    工程初始化

---
+++ src/__init__.py
@@ -0,0 +1,30 @@
+
+message = "hello"
+print(message)
+
+msg = "hEhh'hel'g3wgg"
+print(msg)
+
+# 按照标题栏
+print(msg.title())
+# 大写
+print(msg.upper())
+# 小写
+print(msg.lower())
+
+# 合并字符串
+print(message + " " + msg)
+
+# 制表符添加空白
+msg1 = "I like\t Python"
+print(msg1)
+
+# 换行符
+msg2 = "I love\nPython"
+print(msg2)
+
+# 删除空白
+msg3 = "  I love Python    "
+print(msg3.strip())
+print(msg3.lstrip())
+print(msg3.rstrip())
